185bd89266dbcd95110fb951f073c89cd86a3cbd
James Sral <jamessral@Jamess-MBP.attlocal.net>
2017-06-03 10:14:20 -0400

    Update Emacs config with extra evil

---
+++ elpa/ocp-indent-20170412.12/ocp-indent-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ocp-indent" "20170412.12" "automatic indentation with ocp-indent" 'nil :commit "d3f250b6029a7afec0d7ddd8770d9c4a7e5b9c7c" :url "http://www.typerex.org/ocp-indent.html" :keywords '("ocaml" "languages"))
